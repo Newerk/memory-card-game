a1349e5261ee90d9fe5370b8790836e863538648
newerk <juniusowens@yahoo.com>
2023-10-09 16:07:36 -0400

    Add Loading Animation While Fetching API

--- src/components/gameboard.jsx
+++ src/components/gameboard.jsx
@@ -1,10 +1,16 @@
 import { v4 } from "uuid";
 import "../styling/cards.css";
 import "../styling/gameboard.css";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
-function GenerateCards({ arr, setArr, scoreHandler, gameoverHandler }) {
-  let [nameStorage, setNameStorage] = useState([]);
+function GenerateCards({
+  arr,
+  setArr,
+  scoreHandler,
+  gameoverHandler,
+  loadingRef,
+}) {
+  let [charStorage, setCharStorage] = useState([]);
 
   useEffect(() => {
     const getRandomIndex = (limit, offset, total) => {
@@ -13,39 +19,42 @@ function GenerateCards({ arr, setArr, scoreHandler, gameoverHandler }) {
         : Math.floor(Math.random() * 20);
     };
 
-    if (arr.length < 10) {
+    if (charStorage.length < 10) {
       fetch(
         `https://gateway.marvel.com:443/v1/public/characters?limit=20&offset=${randomOffset()}&apikey=412bd8b1e579c4f5f4139cc8ea699d61`,
         { mode: "cors" }
       ).then((result) => {
         result.json().then((info) => {
           for (let i = 0; i < 5; i++) {
-            // let randomIndex = getRandomIndex(
-            //   info.data.limit,
-            //   info.data.offset,
-            //   info.data.total
-            // );
+            let randomIndex = getRandomIndex(
+              info.data.limit,
+              info.data.offset,
+              info.data.total
+            );
 
             const obj = {
-              name: info.data.results[i].name,
-              path: info.data.results[i].thumbnail.path,
+              name: info.data.results[randomIndex].name,
+              path: info.data.results[randomIndex].thumbnail.path,
               selected: false,
             };
-            console.log(obj.name);
+            console.log(obj.name1);
 
             //if picture if avaliable, created a card, else dont
             if (
-              !info.data.results[i].thumbnail.path.endsWith(
+              !info.data.results[randomIndex].thumbnail.path.endsWith(
                 "image_not_available"
               )
             ) {
-              setArr([...arr, obj]);
+              setCharStorage([...charStorage, obj]);
             }
           }
         });
       });
+    } else {
+      setArr([...charStorage]);
+      loadingRef.current.style.visibility = "hidden";
     }
-  }, [arr]);
+  }, [charStorage]);
 
   function shuffleCards(array) {
     let copy = [...array];
@@ -74,7 +83,6 @@ function GenerateCards({ arr, setArr, scoreHandler, gameoverHandler }) {
 
   return (
     <>
-      {/* <p id="will-delete">Length: {arr.length}</p> */}
       {shuffleCards(arr).map((card) => (
         <div key={v4()}>
           <img
@@ -99,21 +107,15 @@ function GenerateCards({ arr, setArr, scoreHandler, gameoverHandler }) {
 }
 
 export default function Gameboard({ scoreBoardHandler, gameoverHandler }) {
-  const [cardArray, setCardArray] = useState([
-    // { name: "", path: "", selected: false },
-    // { name: "", path: "", selected: false },
-    // { name: "", path: "", selected: false },
-    // { name: "", path: "", selected: false },
-    // { name: "", path: "", selected: false },
-    // { name: "", path: "", selected: false },
-    // { name: "", path: "", selected: false },
-    // { name: "", path: "", selected: false },
-    // { name: "", path: "", selected: false },
-    // { name: "", path: "", selected: false },
-  ]);
+  const [cardArray, setCardArray] = useState([]);
+  const loadingRef = useRef(null);
   return (
     <div id="gameboard">
+      <div ref={loadingRef} className="loading">
+        <div className="spinner"></div>
+      </div>
       <GenerateCards
+        loadingRef={loadingRef}
         arr={cardArray}
         setArr={setCardArray}
         scoreHandler={scoreBoardHandler}
