10fa9baa8f115a4b7be860e43967a2630cc9b91f
newerk <juniusowens@yahoo.com>
2024-05-07 16:53:50 -0400

    Increase App Security

--- src/components/gameboard.jsx
+++ src/components/gameboard.jsx
@@ -25,7 +25,7 @@ function GenerateCards({
     if (charStorage.length < 10) {
       fetch(
         `https://gateway.marvel.com:443/v1/public/characters?limit=20&offset=${randomOffset()}&apikey=${
-          process.env.API_KEY
+          process.env.REACT_APP_API_KEY
         }`,
         { mode: "cors" }
       ).then((result) => {
