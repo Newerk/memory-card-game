55d37922af46dd679a81d5c379c6382f41750988
newerk <juniusowens@yahoo.com>
2024-05-08 11:43:20 -0400

    Remove env variables

--- src/components/gameboard.jsx
+++ src/components/gameboard.jsx
@@ -2,7 +2,6 @@ import { v4 } from "uuid";
 import "../styling/cards.css";
 import "../styling/gameboard.css";
 import { useEffect, useRef, useState } from "react";
-import "dotenv/config";
 
 function GenerateCards({
   arr,
@@ -24,9 +23,7 @@ function GenerateCards({
 
     if (charStorage.length < 10) {
       fetch(
-        `https://gateway.marvel.com:443/v1/public/characters?limit=20&offset=${randomOffset()}&apikey=${
-          import.meta.env.VITE_API_KEY
-        }`,
+        `https://gateway.marvel.com:443/v1/public/characters?limit=20&offset=${randomOffset()}&apikey=412bd8b1e579c4f5f4139cc8ea699d61`,
         { mode: "cors" }
       ).then((result) => {
         result.json().then((info) => {
--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,3 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
 });
-
-// export default defineConfig(({ mode }) => {
-//   const env = loadEnv(mode, process.cwd(), '');
-//   return {
-//     define: {
-//       'process.env.SOME_KEY': JSON.stringify(env.SOME_KEY)
-//     },
-//     plugins: [react()],
-//   }
-// })
